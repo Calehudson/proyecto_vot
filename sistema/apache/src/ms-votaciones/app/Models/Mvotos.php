766c4c8e96c65e6dbdeb0af6703356720b04c734
Carlos León Hudson <cleonh11@gmail.com>
2025-04-21 16:01:37 +0200

    Eliminar carpeta sistema y todo su contenido

--- sistema/apache/src/ms-votaciones/app/Models/Mvotos.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use CodeIgniter\Model;
-
-class Mvotos extends Model {
-    protected $table = 'votos_vot';
-    protected $primaryKey = 'vot_codigo';
-    protected $allowedFields = ['par_codigo', 'vot_ip', 'created_at'];
-}
