07aa252067060a808f4ef9410952b28b2c9bde99
Carlos León Hudson <cleonh11@gmail.com>
2025-04-26 16:20:53 +0200

    Corrección formato ingress-egress

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -56,9 +56,24 @@ resource "aws_security_group" "alb_sg" {
   description = "Permite HTTP 80 y 8000 al ALB"
   vpc_id      = aws_vpc.main.id
 
-  ingress { from_port = 80  to_port = 80   protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
-  ingress { from_port = 8000 to_port = 8000 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
-  egress  { from_port = 0   to_port = 0     protocol = "-1"  cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+    from_port = 80  
+    to_port = 80   
+    protocol = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
+  ingress { 
+    from_port = 8000 
+    to_port = 8000 
+    protocol = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
+  egress  { 
+    from_port = 0   
+    to_port = 0     
+    protocol = "-1"  
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
 }
 
 resource "aws_security_group" "ecs_sg" {
@@ -66,8 +81,18 @@ resource "aws_security_group" "ecs_sg" {
   description = "ECS traffic from ALB"
   vpc_id      = aws_vpc.main.id
 
-  ingress { from_port = 0 to_port = 0 protocol = "-1" security_groups = [aws_security_group.alb_sg.id] }
-  egress  { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+    from_port = 0 
+    to_port = 0 
+    protocol = "-1" 
+    security_groups = [aws_security_group.alb_sg.id] 
+    }
+  egress  { 
+    from_port = 0 
+    to_port = 0 
+    protocol = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
 }
 
 ######################################
@@ -82,8 +107,18 @@ resource "aws_security_group" "db_sg" {
   name   = "vot-${var.environment}-db-sg"
   vpc_id = aws_vpc.main.id
 
-  ingress { from_port = 3306 to_port = 3306 protocol = "tcp" security_groups = [aws_security_group.ecs_sg.id] }
-  egress  { from_port = 0    to_port = 0    protocol = "-1"     cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+    from_port = 3306 
+    to_port = 3306 
+    protocol = "tcp" 
+    security_groups = [aws_security_group.ecs_sg.id] 
+    }
+  egress  { 
+    from_port = 0    
+    to_port = 0    
+    protocol = "-1"     
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
 }
 
 resource "aws_db_instance" "mysql" {
@@ -235,7 +270,12 @@ resource "aws_security_group" "bastion_sg" {
   description = "Seguridad para bastion host (SSM)"
   vpc_id      = aws_vpc.main.id
 
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress { 
+    from_port = 0 
+    to_port = 0 
+    protocol = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
 }
 
 resource "aws_instance" "bastion" {
