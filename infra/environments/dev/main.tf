211a02cf63141e7883ce6dba223f35288f3b0c4a
Carlos León Hudson <cleonh11@gmail.com>
2025-05-02 18:48:45 +0200

    Actualización de group ecs

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -71,6 +71,10 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "ecs_sg" {
