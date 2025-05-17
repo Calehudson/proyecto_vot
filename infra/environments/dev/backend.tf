d531001181aaf9a0b2b9236809667d11998a6e27
Carlos León Hudson <cleonh11@gmail.com>
2025-05-17 21:32:09 +0200

    Añadir workflow CI/CD Terraform

--- infra/environments/dev/backend.tf
+++ infra/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "proyecto-vot-terraform-state-2025-5"
+    bucket         = "proyecto-vot-terraform-state-2025-55"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "proyecto-vot-terraform-locks"
