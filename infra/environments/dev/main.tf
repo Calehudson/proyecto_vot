2fe68f23e0e89e265a4b53ab98da2b3c9b65843f
Carlos León Hudson <cleonh11@gmail.com>
2025-05-18 20:17:28 +0200

    Actualización de rutas FD

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -390,7 +390,7 @@ resource "aws_ecs_task_definition" "frontend" {
   container_definitions = jsonencode([
     {
       name         = "frontend"
-      image        = "calehu/frontend:v2.7"
+      image        = "calehu/frontend:v2.8"
       portMappings = [{ containerPort = 80, protocol = "tcp" }]
     }
   ])
