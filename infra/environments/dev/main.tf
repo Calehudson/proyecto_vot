0cce517a0fdec163c3193c05c4152fd1be85a1b0
Carlos León Hudson <cleonh11@gmail.com>
2025-05-18 12:09:59 +0200

    Depuración de tags

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -165,8 +165,12 @@ resource "aws_lb" "main" {
   security_groups    = [aws_security_group.alb_sg.id]
 }
 
+resource "random_id" "frontend_tg" {
+  byte_length = 2   # 2 bytes → 4 dígitos hexadecimales
+}
+
 resource "aws_lb_target_group" "frontend_tg" {
-  name_prefix = "frontend-${var.environment}-tg-"
+  name        = "frontend-${var.environment}-tg-${random_id.frontend_tg.hex}"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
