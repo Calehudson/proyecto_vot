7453504c997f654280e91bbc6fba12a53c96dcf0
Carlos León Hudson <cleonh11@gmail.com>
2025-05-21 18:00:24 +0200

    Despliegue final del sistema

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -517,7 +517,7 @@ resource "aws_ecs_task_definition" "grafana" {
   }])
 }
 
-# Service de grafana - prx
+# Service de grafana - prx 25
 resource "aws_ecs_service" "grafana" {
   name            = "grafana"
   cluster         = aws_ecs_cluster.main.id
